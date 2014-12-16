236850f3fa57047151c5ad22cfcf4728fb007e9c
Todd Eichel <todd@toddeichel.com>
2014-12-16 00:07:41 -0800

    Fix config parser for integer settings.

--- settings.py
+++ settings.py
@@ -10,5 +10,5 @@ class Settings():
 		self.s3_access_key = parser.get(SECTION, 'S3_ACCESS_KEY')
 		self.s3_secret_key = parser.get(SECTION, 'S3_SECRET_KEY')
 		self.s3_bucket_name = parser.get(SECTION, 's3_bucket_name')
-		self.s3_max_file_size = parser.get(SECTION, 's3_max_file_size')
-		self.s3_max_file_part_size = parser.get(SECTION, 's3_max_file_part_size')
+		self.s3_max_file_size = parser.getint(SECTION, 's3_max_file_size')
+		self.s3_max_file_part_size = parser.getint(SECTION, 's3_max_file_part_size')
