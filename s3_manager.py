ace5b16d65becf4121a319d07dc65da0981bb7fd
Todd Eichel <todd@toddeichel.com>
2014-12-16 00:08:01 -0800

    Tweak upload status output formatting.

--- s3_manager.py
+++ s3_manager.py
@@ -36,25 +36,24 @@ def upload(connection, bucket, folder):
 			path_prefix = '/'.join(path_parts[2:])
 			destpath = os.path.join(path_prefix, filename)
 			final_dest_path = os.path.join(destDir, destpath)
-			print 'Uploading %s to Amazon S3 bucket %s' % (filename, bucket.name)
+			sys.stdout.write('Uploading %s to Amazon S3 bucket %s' % (filename, bucket.name))
 	 
 			filesize = os.path.getsize(sourcepath)
 			if filesize > settings.s3_max_file_size:
-				print "Starting multipart upload"
 				mp = bucket.initiate_multipart_upload(final_dest_path)
 				fp = open(sourcepath,'rb')
 				fp_num = 0
 				while (fp.tell() < filesize):
 					fp_num += 1
-					print "Uploading part %i" % fp_num
+					sys.stdout.write("\nUploading part %i" % fp_num)
 					mp.upload_part_from_file(fp, fp_num, cb=_percent_complete, num_cb=10, size=settings.s3_max_file_part_size)
 
 				mp.complete_upload()
 		 
 			else:
-				print "Uploading in one part"
 				k = boto.s3.key.Key(bucket)
 				k.key = final_dest_path
 				k.set_contents_from_filename(sourcepath, cb=_percent_complete, num_cb=10)
+				print
 	print 
 
